665892fc7932d8090daedc7705e54903caa5fad6
pihedy <pihedy@gmail.com>
2024-01-31 18:58:38 +0100

    Add options and interface functions.

--- src/app/Component/Console/Input/AbstractInput.php
+++ src/app/Component/Console/Input/AbstractInput.php
@@ -6,7 +6,47 @@ abstract class AbstractInput implements InputInterface
 {
     protected array $data = [];
 
+    protected array $options = [];
+
     protected array $arguments = [];
 
     abstract protected function parse(): void;
+
+    public function getArgument(string $name): ?string
+    {
+        if (!$this->hasArgument($name)) {
+            return null;
+        }
+
+        return $this->arguments[$name];
+    }
+
+    public function getArguments(): array
+    {
+        return $this->arguments;
+    }
+
+    public function getOption(string $name)
+    {
+        if (!$this->hasOption($name)) {
+            return null;
+        }
+
+        return $this->options[$name];
+    }
+
+    public function getOptions(): array
+    {
+        return $this->options;
+    }
+
+    public function hasArgument(string $name): bool
+    {
+        return isset($this->arguments[$name]);
+    }
+
+    public function hasOption(string $name): bool
+    {
+        return isset($this->options[$name]);
+    }
 }
