6aa00223efdfe84959cc25e35eae530fd04ddade
pihedy <pihedy@gmail.com>
2024-08-13 22:40:29 +0200

    A simple exception in case device is open and cannot be modified.

---
+++ src/app/Components/Serial/Exception/DeviceAlreadyOpenedException.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace RcNetwork\Components\Serial\Exception;
+
+/**
+ * Exception thrown when attempting to open a serial device that is already open.
+ *
+ * @author Pihe Edmond <pihedy@gmail.com>
+ */
+class DeviceAlreadyOpenedException extends \Exception
+{
+    /* Do Nothing */
+}
